7367faf283a0a621f2791504078cfd92842ed3c2
ThyChristiansen <thyvu0411@gmail.com>
2020-08-27 10:19:20 -0500

    set up buttons for more vert icon

--- src/components/RecipeDetail/RecipeDetail.js
+++ src/components/RecipeDetail/RecipeDetail.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { withStyles } from '@material-ui/core/styles';
-import { Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography, Container } from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, IconButton, Typography, Container,Popover,Chip, List, ListItem, ListItemIcon,ListItemText,Divider } from '@material-ui/core';
+// import {MoreVertIcon,ShareIcon} from '@material-ui/icons';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-
-import Chip from '@material-ui/core/Chip';
-
-import Grid from '@material-ui/core/Grid';
 
 const useStyles = (theme) => ({
   root: {
@@ -19,15 +17,42 @@ const useStyles = (theme) => ({
     height: 0,
     paddingTop: '56.25%',
   },
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
 });
 
-
 class RecipeDetail extends Component {
+  state = {
+    anchorEl: '',
+    open: false,
+  }
+  handleOpen = (event) => {
+    this.setState({
+      anchorEl: event.currentTarget,
+      open: true,
+    })
+    console.log('clicked')
+  };
 
+  handleClose = () => {
+    this.setState({
+      anchorEl: null,
+      open: false,
+    })
+  };
+  ListItemLink = (props) => {
+    return <ListItem button component="a" {...props} />;
+  }
 
   render() {
 
     const { classes } = this.props;
+
+    const id = this.state.open ? 'simple-popover' : undefined;
     return (
       <Container maxWidth="md">
         {this.props.recipe.map((item) => {
@@ -36,7 +61,46 @@ class RecipeDetail extends Component {
               <CardHeader
                 action={
                   <IconButton aria-label="settings">
-                    <MoreVertIcon />
+                    <MoreVertIcon aria-describedby={id}
+                      onClick={this.handleOpen}
+                    />
+                    <Popover
+                      id={id}
+                      open={this.state.open}
+                      anchorEl={this.state.anchorEl}
+                      onClose={this.handleClose}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                      }}
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                      }}
+                    >
+                      <List component="nav" aria-label="main mailbox folders">
+                        <ListItem button>
+                          <ListItemIcon>
+                            <FavoriteBorderIcon />
+
+                          </ListItemIcon>
+                          <ListItemText primary="Add to favorite" />
+                        </ListItem>
+                        <ListItem button>
+                          <ListItemIcon>
+                            <ShareIcon />
+                          </ListItemIcon>
+                          <ListItemText primary="Share" />
+                        </ListItem>
+                        <ListItem button>
+                          <ListItemIcon>
+                            <CalendarTodayIcon />
+                          </ListItemIcon>
+                          <ListItemText primary="Add to calender" />
+                        </ListItem>
+                      </List>
+                      <Divider />
+                    </Popover>
                   </IconButton>
                 }
                 title={item.title}
@@ -53,16 +117,7 @@ class RecipeDetail extends Component {
                 {/* <Typography variant="body2" color="textSecondary" component="p">
                 </Typography> */}
               </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-
-                </IconButton>
-              </CardActions>
-              {/* <Collapse in={expanded} timeout="auto" unmountOnExit> */}
+              
               <CardContent>
                 <Typography paragraph>Ingreadients: </Typography>
                 {item.extendedIngredients.map((i) => {
