42784b35e0e80074836b184ff8c71c30e3273621
ThyChristiansen <thyvu0411@gmail.com>
2020-09-08 21:18:50 -0500

    styling for search sidebar

--- src/components/Recipes/FindRecipes.jsx
+++ src/components/Recipes/FindRecipes.jsx
@@ -4,10 +4,11 @@ import { connect } from 'react-redux';
 import RecipeDetail from '../RecipeDetail/RecipeDetail';
 
 import { fade, withStyles } from '@material-ui/core/styles';
-import { Grid, Container, Typography, InputBase, FormControl, InputLabel, Select, Input, MenuItem, Checkbox, ListItemText, Chip, FormControlLabel, Switch, Card, CardHeader, Table, TableBody, TableRow, TableCell, CardMedia, CardActionArea, CardContent, CardActions, Button, Slider } from '@material-ui/core';
+import { Grid, Container, Typography, InputBase, FormControl, InputLabel, Select, Input, MenuItem, Checkbox, ListItemText, Chip, FormControlLabel, Switch, Card, CardHeader, Table, TableBody, TableRow, TableCell, CardMedia, CardActionArea, CardContent, CardActions, Button, Slider, List, ListSubheader, ListItem, Collapse } from '@material-ui/core';
 import './Recipes.css'
 import SearchIcon from '@material-ui/icons/Search';
-
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 const useStyles = (theme) => ({
   root: {
@@ -17,8 +18,17 @@ const useStyles = (theme) => ({
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
-    backgroundColor: 'lightgray',
+    // backgroundColor: 'lightgray',
+
   },
+  sideSearchBar: {
+    backgroundColor: 'lightgray',
+    paddingLeft: "20px",
+    borderRadius: "10px",
+    paddingRight: theme.spacing(2),
+
+  },
+
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
@@ -74,6 +84,12 @@ const useStyles = (theme) => ({
   card: {
     height: "100%",
     wight: "100%",
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+  nutritionName:{
+    paddingRight: theme.spacing(5),
 
   }
 
@@ -103,6 +119,7 @@ class FindRecipes extends Component {
     fat: [5, 100],
     protein: [5, 100],
     intolerances: ["none"],
+    expan: true
   }
 
   componentDidMount() {
@@ -248,195 +265,221 @@ class FindRecipes extends Component {
     })
   }
 
+  handleExpan = () => {
+    this.setState({
+      expan: !this.state.expan
+    })
+  }
+
   render() {
     const { classes, reduxState } = this.props;
 
     return (
-      <Container maxWidth="md" className={classes.root}  >
-        <div className={classes.search}>
-          <div className={classes.searchIcon}>
-            <SearchIcon />
-          </div>
-          <InputBase
-            value={this.state.input}
-            placeholder="Search..."
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput,
-            }}
-            inputProps={{ 'aria-label': 'search' }}
-            onChange={this.handleInputOnChange}
-          />
-        </div>
+      <Container className={classes.root}  >
 
+        <Grid container spacing={2} >
 
-        <Grid container spacing={2}>
-          <Grid item xs={3} >
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-mutiple-checkbox-label">Meal Options</InputLabel>
-              <Select
-                labelId="demo-mutiple-checkbox-label"
-                id="demo-mutiple-checkbox"
-                value={this.state.meal}
-                onChange={this.handleMealChange}
-              >
-                {['breakfast',
-                  'main course',
-                  'dessert',
-                  'drink',
-                  'salad',
-
-                ].map((name) => (
-                  <MenuItem key={name} value={name}>
-                    {name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-
-          <Grid item xs={3} >
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-mutiple-checkbox-label">Diet</InputLabel>
-              <Select
-                labelId="demo-mutiple-checkbox-label"
-                id="demo-mutiple-checkbox"
-                value={this.state.diet}
-                onChange={this.handleDietChange}
-              >
-                {['none',
-                  'pescetarian',
-                  'lacto vegetarian',
-                  'ovo vegetarian',
-                  'vegan',
-                  'paleo',
-                  'primal',
-                  'vegetarian'
-
-                ].map((name) => (
-                  <MenuItem key={name} value={name}>
-                    {name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-
-          <Grid item xs={3} >
-            <Typography id="range-slider" gutterBottom>Calories</Typography>
-            <Slider
-              value={this.state.calories}
-              min={150}
-              step={10}
-              max={1500}
-              getAriaValueText={(value) => { return `${value}` }}
-              onChange={this.handleCaloriesChange}
-              valueLabelDisplay="auto"
-              aria-labelledby="range-slider"
-            />
-          </Grid>
-
-          <Grid item xs={3} >
-            <Typography id="range-slider" gutterBottom>Fat</Typography>
-            <Slider
-              value={this.state.fat}
-              min={5}
-              step={10}
-              max={100}
-              getAriaValueText={(value) => { return `${value}` }}
-              onChange={this.handleFatChange}
-              valueLabelDisplay="auto"
-              aria-labelledby="range-slider"
-            />
-          </Grid>
-
-          <Grid item xs={3} >
-            <Typography id="range-slider" gutterBottom>Protein</Typography>
-            <Slider
-              value={this.state.protein}
-              min={5}
-              step={10}
-              max={100}
-              getAriaValueText={(value) => { return `${value}` }}
-              onChange={this.handleProteinChange}
-              valueLabelDisplay="auto"
-              aria-labelledby="range-slider"
-            />
-          </Grid>
-
-          <Grid item xs={3} >
-            <FormControl className={classes.formControl}>
-              <InputLabel id="demo-mutiple-checkbox-label">Exclude Ingredients Options</InputLabel>
-              <Select
-                labelId="demo-mutiple-chip-label"
-                id="demo-mutiple-chip"
-                multiple
-                value={this.state.intolerances}
-                onChange={this.handleIntolerancesChange}
-                input={<Input id="select-multiple-chip" />}
-                renderValue={(selected) => (
-                  <div className={classes.chips}>
-                    {selected.map((value) => (
-                      <Chip key={value} label={value} className={classes.chip} />
-                    ))}
-                  </div>
-                )}
-                MenuProps={MenuProps}
-
-              >
-                {["none","dairy", "egg","gluten", "peanut","sesame", "seafood", "shellfish", "soy"," sulfite","tree nut", "wheat"].map((name) => (
-                  <MenuItem key={name} value={name} >
-                    {name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-
-          <Grid item xs={3} >
-            <FormControlLabel
-              label="My recipes"
-              control={
-                <Switch
-                  checked={this.state.myRecipes}
-                  onChange={this.handleSwitchChange}
-                  name="myRecipes"
-                  color="primary"
-                />
+          <Grid item xs={3} className={classes.sideSearchBar}>
+            <List
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+              subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                  Search your recipes</ListSubheader>
               }
-            />
+            >
+              <ListItem >
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    value={this.state.input}
+                    placeholder="Search..."
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'search' }}
+                    onChange={this.handleInputOnChange}
+                  />
+                </div>
+              </ListItem>
+              <ListItem >
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-mutiple-checkbox-label">Meal Options</InputLabel>
+                  <Select
+                    labelId="demo-mutiple-checkbox-label"
+                    id="demo-mutiple-checkbox"
+                    value={this.state.meal}
+                    onChange={this.handleMealChange}
+                  >
+                    {['breakfast',
+                      'main course',
+                      'dessert',
+                      'drink',
+                      'salad',
+
+                    ].map((name) => (
+                      <MenuItem key={name} value={name}>
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </ListItem>
+             
+              <ListItem onClick={this.handleExpan}>
+                <ListItemText primary="Nutritions" />
+                {this.state.expan ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.expan} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem className={classes.nested}>
+                    <Typography id="range-slider" gutterBottom className={classes.nutritionName}>Fat</Typography>
+                    <Slider
+                      value={this.state.fat}
+                      min={5}
+                      step={10}
+                      max={100}
+                      getAriaValueText={(value) => { return `${value}` }}
+                      onChange={this.handleFatChange}
+                      valueLabelDisplay="auto"
+                      aria-labelledby="range-slider"
+                    />
+                  </ListItem>
+                  <ListItem className={classes.nested}>
+                    <Typography id="range-slider" gutterBottom className={classes.nutritionName}>Protein</Typography>
+                    <Slider
+                      value={this.state.protein}
+                      min={5}
+                      step={10}
+                      max={100}
+                      getAriaValueText={(value) => { return `${value}` }}
+                      onChange={this.handleProteinChange}
+                      valueLabelDisplay="auto"
+                      aria-labelledby="range-slider"
+                    />
+                  </ListItem>
+                  <ListItem className={classes.nested}>
+                    <Typography id="range-slider" gutterBottom className={classes.nutritionName}>Calories</Typography>
+                    <Slider
+                      value={this.state.calories}
+                      min={150}
+                      step={10}
+                      max={1500}
+                      getAriaValueText={(value) => { return `${value}` }}
+                      onChange={this.handleCaloriesChange}
+                      valueLabelDisplay="auto"
+                      aria-labelledby="range-slider"
+                    />
+                  </ListItem>
+                </List>
+              </Collapse>
+            </List>
+
+            <ListItem>
+              <FormControl className={classes.formControl}>
+                <InputLabel id="demo-mutiple-checkbox-label">Diet</InputLabel>
+                <Select
+                  labelId="demo-mutiple-checkbox-label"
+                  id="demo-mutiple-checkbox"
+                  value={this.state.diet}
+                  onChange={this.handleDietChange}
+                >
+                  {['none',
+                    'pescetarian',
+                    'lacto vegetarian',
+                    'ovo vegetarian',
+                    'vegan',
+                    'paleo',
+                    'primal',
+                    'vegetarian'
+                  ].map((name) => (
+                    <MenuItem key={name} value={name}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </ListItem>
+
+            <ListItem>
+              <FormControl className={classes.formControl}>
+                <InputLabel id="demo-mutiple-checkbox-label">Exclude Ingredients Options</InputLabel>
+                <Select
+                  labelId="demo-mutiple-chip-label"
+                  id="demo-mutiple-chip"
+                  multiple
+                  value={this.state.intolerances}
+                  onChange={this.handleIntolerancesChange}
+                  input={<Input id="select-multiple-chip" />}
+                  renderValue={(selected) => (
+                    <div className={classes.chips}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} className={classes.chip} />
+                      ))}
+                    </div>
+                  )}
+                  MenuProps={MenuProps}
+
+                >
+                  {["none", "dairy", "egg", "gluten", "peanut", "sesame", "seafood", "shellfish", "soy", " sulfite", "tree nut", "wheat"].map((name) => (
+                    <MenuItem key={name} value={name} >
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </ListItem>
+
+            <ListItem>
+              <FormControlLabel
+                label="My recipes"
+                control={
+                  <Switch
+                    checked={this.state.myRecipes}
+                    onChange={this.handleSwitchChange}
+                    name="myRecipes"
+                    color="primary"
+                  />
+                }
+              />
+            </ListItem>
           </Grid>
+          <Grid item xs={9} >
+            <Grid container spacing={2}>
+              {reduxState.getRecipeReducer.map((item) => {
+                return (
+                  <>
+                    <Grid item xs={3}>
+                      <Card className={classes.card}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.media}
+                            image={item.image}
+                            title="Contemplative Reptile"
+                          />
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                              {item.title}
+                            </Typography>
+
+                          </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  </>
+                )
+              })}
+            </Grid>
+          </Grid>
+
         </Grid>
 
-        <Grid container spacing={2}>
-
-          {reduxState.getRecipeReducer.map((item) => {
-            return (
-              <>
-                <Grid item xs={3}>
-                  <Card className={classes.card}>
-                    <CardActionArea>
-                      <CardMedia
-                        className={classes.media}
-                        image={item.image}
-                        title="Contemplative Reptile"
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {item.title}
-                        </Typography>
-
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-
-                    </CardActions>
-                  </Card>
-                </Grid>
-              </>
-            )
-          })}
-        </Grid>
       </Container>
     )
   }
