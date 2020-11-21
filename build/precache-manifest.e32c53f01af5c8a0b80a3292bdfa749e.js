0fb07606ece24f96d70f7b87e02d40ee485c8233
Thy Christiansen <thyvu0411@gmail.com>
2020-11-21 17:21:20 -0600

    update

--- build/precache-manifest.e32c53f01af5c8a0b80a3292bdfa749e.js
+++ build/precache-manifest.e32c53f01af5c8a0b80a3292bdfa749e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d30ff2bf09ef90e3d0602eeb947312be",
+    "revision": "7fa345808173c13d83e8314e308569f8",
     "url": "/food-and-nutrition/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/food-and-nutrition/static/css/2.08a81a33.chunk.css"
   },
   {
-    "revision": "9f6fad651302bbbff731",
+    "revision": "c6e59c789fc5161e9c80",
     "url": "/food-and-nutrition/static/css/main.0a688ac8.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/food-and-nutrition/static/js/2.b9d4081e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f6fad651302bbbff731",
-    "url": "/food-and-nutrition/static/js/main.f9a11e53.chunk.js"
+    "revision": "c6e59c789fc5161e9c80",
+    "url": "/food-and-nutrition/static/js/main.90606942.chunk.js"
   },
   {
     "revision": "c0d2aff4dcdcb92aa5ad",
