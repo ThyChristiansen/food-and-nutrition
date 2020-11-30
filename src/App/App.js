d7e44f6c3cb112b89b6737c535d2b4bc76e05199
Thy Christiansen <thyvu0411@gmail.com>
2020-11-30 15:21:17 -0600

    run build

--- src/App/App.js
+++ src/App/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   HashRouter as Router,
   Route,
   Redirect,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import Nav from '../components/Nav/Nav';
-import Footer from '../components/Footer/Footer';
+import Nav from "../components/Nav/Nav";
+import Footer from "../components/Footer/Footer";
 
-import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute'
-import InfoPage from '../components/InfoPage/InfoPage';
-import LandingPage from '../pages/LandingPage';
-import Recipes from '../pages/RecipesMainPage';
-import RecipeDetailPage from '../pages/RecipeDetailPage';
-import './App.css';
-import LoginPage from '../pages/LoginPage';
-import RegisterPage from '../pages/RegisterPage';
-import FindRecipes from '../pages/FindRecipes';
-import RandomRecipeList from '../components/Recipes/RandomRecipeList';
-import FavoriteList from '../pages/FavoriteList';
-import Calendar from '../pages/Calendar/CalendarPage';
-import PaymentKeepTrack from '../pages/PaymentKeepTrack';
-
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import LandingPage from "../pages/LandingPage";
+import Recipes from "../pages/RecipesMainPage";
+import RecipeDetailPage from "../pages/RecipeDetailPage";
+import "./App.css";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import FindRecipes from "../pages/FindRecipes";
+import RandomRecipeList from "../components/Recipes/RandomRecipeList";
+import FavoriteList from "../pages/FavoriteList";
+import Calendar from "../pages/Calendar/CalendarPage";
+import PaymentKeepTrack from "../pages/PaymentKeepTrack";
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch({ type: 'FETCH_USER' })
+    this.props.dispatch({ type: "FETCH_USER" });
   }
 
   render() {
@@ -41,38 +39,13 @@ class App extends Component {
             <Redirect exact from="/" to="/home" />
             {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/home"
-              component={LandingPage}
-            />
-            <Route
-              exact
-              path="/sign-in"
-              component={LoginPage}
-            />
-            <Route
-              exact
-              path="/sign-up"
-              component={RegisterPage}
-            />
+            <Route exact path="/home" component={LandingPage} />
+            <Route exact path="/sign-in" component={LoginPage} />
+            <Route exact path="/sign-up" component={RegisterPage} />
 
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={LandingPage}
-            />
+            <ProtectedRoute exact path="/home" component={LandingPage} />
 
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
-            <ProtectedRoute
-              exact
-              path="/recipes"
-              component={Recipes}
-            />
+            <ProtectedRoute exact path="/recipes" component={Recipes} />
             <ProtectedRoute
               exact
               path="/find-recipes"
@@ -89,26 +62,19 @@ class App extends Component {
               component={RandomRecipeList}
             />
 
-            <ProtectedRoute
-              path="/calendar"
-              component={Calendar}
-            />
-            <ProtectedRoute
-              path="/favorite-recipes"
-              component={FavoriteList}
-            />
+            <ProtectedRoute path="/calendar" component={Calendar} />
+            <ProtectedRoute path="/favorite-recipes" component={FavoriteList} />
             <ProtectedRoute
               path="/payment-keep-track"
               component={PaymentKeepTrack}
             />
-           
-            <Route render={() => <h1>404</h1>} />
 
+            <Route render={() => <h1>404</h1>} />
           </Switch>
           <Footer />
         </div>
       </Router>
-    )
+    );
   }
 }
 
